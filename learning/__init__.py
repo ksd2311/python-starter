3cd64787ea25e1ab03cf47a4132f4ae3bd752fce
Ajayk-dev <ajay.k.bawa@outlook.com>
2021-05-01 12:38:28 +0530

    test

---
+++ learning/__init__.py
@@ -0,0 +1,9 @@
+
+class Test(object):
+    def __init__(self,name,age):
+        self.age = age
+        self.name = name
+print("Hello World")
+test = Test("Test", 29)
+print(test.age)
+print(test.name)
